baa55dc6ca3475ce7df25012d477adbeda6c66bb
Julio Cesar Silva de Oliveira <julioosilva97@ibm.com>
2021-05-24 20:55:04 -0300

    Testes unitários da aplicação

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,28 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DateUtilsTest {
+	
+	@Test
+	public void deveRetornarTrueDatasFuturas() {
+		LocalDate date = LocalDate.of(2030, 01, 01);
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));	
+	}
+	
+	@Test
+	public void deveRetornarFalseDatasFuturas() {
+		LocalDate date = LocalDate.of(2010, 01, 01);
+		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));	
+	}
+	
+	@Test
+	public void deveRetornarTrueDatasAtuais() {
+		LocalDate date = LocalDate.now();
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));	
+	}
+
+}
